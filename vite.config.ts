5d45c33f7ee15c229d86d2953bc6dfe874101bb5
Thomas <1665577+thurti@users.noreply.github.com>
2026-02-13 15:32:17 +0100

    add osman ntl-2col styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 // @ts-ignore
-import path from 'path';
+import path from "path";
 import visualizer from "rollup-plugin-visualizer";
 
 export default defineConfig({
     plugins: [
         tsconfigPaths(),
         {
-            name: 'watch-md-reload',
+            name: "watch-md-reload",
             handleHotUpdate({ file, server }) {
-                if (file.endsWith('.md')) {
-                    server.ws.send({ type: 'full-reload' });
+                if (file.endsWith(".md")) {
+                    server.ws.send({ type: "full-reload" });
                 }
             },
         },
     ],
     server: {
         port: 4000,
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         hmr: true,
     },
     root: __dirname,
+    resolve: {
+        alias: {
+            "@leuffen/themejs2": path.resolve(__dirname),
+        },
+    },
     build: {
         minify: false,
         cssCodeSplit: false,
@@ -29,26 +34,25 @@ export default defineConfig({
             plugins: [
                 visualizer({
                     filename: "docs/assets/dist/stats.html",
-                    open: true,          // auto-open in browser
+                    open: true, // auto-open in browser
                     gzipSize: true,
                     brotliSize: true,
                     template: "treemap", // or: "sunburst", "network"
                 }),
             ],
             input: {
-                index: path.resolve(__dirname, 'docs/_src/index.ts'),
-                embed: path.resolve(__dirname, 'docs/_src/embed.ts'),
+                index: path.resolve(__dirname, "docs/_src/index.ts"),
+                embed: path.resolve(__dirname, "docs/_src/embed.ts"),
             },
             output: {
-
                 assetFileNames: (assetInfo) => {
-                    if (assetInfo.name && assetInfo.name.endsWith('.css')) {
-                        return '[name][extname]';
+                    if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+                        return "[name][extname]";
                     }
-                    return '[name][extname]';
+                    return "[name][extname]";
                 },
-                entryFileNames: '[name].js',
-                dir: 'docs/assets/dist',
+                entryFileNames: "[name].js",
+                dir: "docs/assets/dist",
             },
         },
     },
